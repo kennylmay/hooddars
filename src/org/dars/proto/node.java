e0f70be782c343728721dbc33c70c0b3f908aa4d
kresss <kresss@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-10-25 23:13:07 +0000

    Fixing some style items.

--- src/org/dars/proto/node.java
+++ src/org/dars/proto/node.java
@@ -7,34 +7,35 @@ package org.dars.proto;
  * Base Class for all protocol specific node types.
  * 
  * @author kresss
- *
+ * 
  */
-public abstract interface node {
+public abstract interface Node {
 
 	/**
 	 * Send raw message into the network.
 	 * 
-	 * This function sends a pre-formatted message into the network.  
-	 * A raw message should be thought of as the bits on a wire 
-	 *  not text.
+	 * This function sends a pre-formatted message into the network. A raw
+	 * message should be thought of as the bits on a wire not text.
 	 * 
 	 * @author kresss
 	 * 
-	 * @param message Pre-formatted message to be transmitted.
+	 * @param message
+	 *            Pre-formatted message to be transmitted.
 	 */
 	void sendRawMessage();
-	
+
 	/**
 	 * Receive raw message from the network.
 	 * 
-	 * This function will receive a message from the network then
-	 *  determine what type of message it is and call the appropriate 
-	 *  message processing routine.
-	 *  
+	 * This function will receive a message from the network then determine what
+	 * type of message it is and call the appropriate message processing
+	 * routine.
+	 * 
 	 * @author kresss
 	 * 
-	 * @param message Message from the network simulation engine.
+	 * @param message
+	 *            Message from the network simulation engine.
 	 */
 	void receiveRawMessage();
-	
+
 }
