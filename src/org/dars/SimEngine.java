8c8a2b4edc59a7aac7ca6d7cb6d6b7a92622fe9d
kennylmay <kennylmay@gmail.com>
2010-10-25 23:40:27 +0000

    Adding SimEngine

---
+++ src/org/dars/SimEngine.java
@@ -0,0 +1,98 @@
+package org.dars;
+
+/**
+ * @author Kenny
+ * 
+ */
+public class SimEngine {
+	/**
+	 * Time to wait for an iteration.
+	 */
+	private int WAIT_TIME = 1;
+
+	/**
+	 * Function that will start a simulation
+	 * 
+	 * This function will be the controlling function for all of the nodes
+	 * 
+	 * @author kennylmay
+	 * 
+	 * @param
+	 */
+	void runSimulation() {
+
+	}
+
+	/**
+	 * Function that sets the timer speed
+	 * 
+	 * This method will allow the adjustment of the time interval(time between
+	 * clock ticks) in seconds.
+	 * 
+	 * @author kennylmay
+	 * 
+	 * @param speed
+	 *            (int) The time in seconds that a simulation should pause
+	 *            between ticks.
+	 */
+	void setSimulationSpeed(int speed) {
+		set_speed(speed);
+	}
+	
+	/**
+	 * Function that will pause a simulation
+	 * 
+	 * This function will pause the simulation until the user decides to
+	 * continue
+	 * 
+	 * @author kennylmay
+	 * 
+	 * @param
+	 */
+	void pauseSimluation() {
+
+	}
+
+	/**
+	 * Function that will stop a simulation
+	 * 
+	 * This function will stop a simulation indefinitely.
+	 * 
+	 * @author kennylmay
+	 * 
+	 * @param
+	 */
+	void stopSimluation() {
+
+	}
+
+	/**
+	 * Function that sets the timer speed
+	 * 
+	 * This method will allow the adjustment of the time interval(time between
+	 * clock ticks) in seconds.
+	 * 
+	 * @author kennylmay
+	 * 
+	 * @param speed
+	 *            (int) The time in seconds that a simulation should pause
+	 *            between ticks.
+	 */
+	public void set_speed(int speed) {
+		WAIT_TIME = speed;
+	}
+
+	/**
+	 * Function that returns the timer speed
+	 * 
+	 * This method will return the wait time.
+	 * 
+	 * @author kennylmay
+	 * 
+	 * @param 
+	 */
+	public int get_speed() {
+		return WAIT_TIME;
+	}
+
+}
