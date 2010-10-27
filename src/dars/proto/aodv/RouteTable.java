8bf170527f7fd555b94f7268e3c7028b60679d6a
kresss <kresss@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-10-27 02:30:02 +0000

    Some small changes to RouteTable and a new RouteEntry Class.  Also updated the Doxyfile to handle the newly refactored directory structure.

--- src/dars/proto/aodv/RouteTable.java
+++ src/dars/proto/aodv/RouteTable.java
@@ -1,14 +1,15 @@
 package dars.proto.aodv;
 
-
 /**
  * AODV Node Routing Table
  * 
- * This class implements a routing table for an AODV Node.
+ * This class implements a routing table for an AODV Node. The routing table is
+ * a collection of Route Entries.
  * 
  * @author kresss
- *
+ * 
  */
 public class RouteTable {
 
+	// THis should be a hashtable?(on Dest IP) of route entires
 }
