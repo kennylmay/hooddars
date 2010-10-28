d1c956395bf6ae93e4b66db54bc82859e904ef31
memjjay <memjjay@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-10-28 18:40:36 +0000

    adding some optimizations to drawing. Also adding some event classes

---
+++ trunk/src/dars/event/InputAddNode.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dars.event;
+import dars.NodeAttributes;
+/**
+ * @author moormanm
+ *
+ */
+public class InputAddNode extends DARSEvent {
+
+  public InputAddNode(NodeAttributes nodeAttributes) {
+    //stub
+  }
+  
+  public String getLogString() {
+    return "ASDF";
+  }
+}
