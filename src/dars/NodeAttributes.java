57ca684bf162ed93c7d1b39c4bafaa60652baa9d
kennylmay <kennylmay@gmail.com>
2010-10-30 16:39:25 +0000

    Updating Sim engine and Node store to work with node and aodv

--- src/dars/NodeAttributes.java
+++ src/dars/NodeAttributes.java
@@ -1,5 +1,7 @@
 package dars;
 
 public class NodeAttributes {
-
+	public int locationx;
+	public int locationy;
+	public int range;
 }
--- src/dars/SimEngine.java
+++ src/dars/SimEngine.java
@@ -6,115 +6,119 @@ import javax.swing.Timer;
 
 import dars.NodeStore;
 import dars.proto.*;
+
 /**
  * @author Kenny
  * 
  */
 public class SimEngine {
-	/**
-	 * Time to wait for an iteration.
-	 */
-	private int WAIT_TIME = 1000;
-	/**
-	 * Boolean that is used to keep track of the state of the simulation
-	 */
-    private boolean paused = false;
-    private NodeStore<Object> store = new NodeStore<Object>();
-    
-    ActionListener engine = new ActionListener() {
-	      public void actionPerformed(ActionEvent evt) {
-	         /// Issue a clock tick to each node in the node store.
-	    	 for(int index = 0; index < store.getNumberOfNodes(); index++){
-	    		Object i = store.getNodeByIndex(index);
-	    	 }
-	      }
-	};
-	
-	/**
-	 * Timer that will control the execution of the simulation
-	 */
-	Timer timer = new Timer(WAIT_TIME, engine);
-		
-	/**
-	 * Function that will start a simulation
-	 * 
-	 * This function will be the controlling function for all of the nodes
-	 * 
-	 * @author kennylmay
-	 * 
-	 * @param
-	 */
-	void runSimulation() {
-		timer.start();
-	}
+/**
+ * Time to wait for an iteration.
+ */
+private int WAIT_TIME = 1000;
+/**
+ * Boolean that is used to keep track of the state of the simulation
+ */
+private boolean paused = false;
+private NodeStore store = new NodeStore();
 
-	/**
-	 * Function that sets the timer speed
-	 * 
-	 * This method will allow the adjustment of the time interval(time between
-	 * clock ticks) in seconds.
-	 * 
-	 * 
-	 * @author kennylmay
-	 * 
-	 * @param speed
-	 *            (int) The time in seconds that a simulation should pause
-	 *            between ticks.
-	 */
-	void setSimulationSpeed(int speed) {
-		timer.setDelay(speed);
-	}
-	
-	/**
-	 * Function that will pause a simulation
-	 * 
-	 * This function will pause the simulation until the user decides to
-	 * continue
-	 * 
-	 * @author kennylmay
-	 * 
-	 * @param
-	 */
-	void pauseSimluation() {
-		if (paused == false){
-			try {
-				timer.wait();
-			} catch (InterruptedException e) {
-				/// May have to change this catch later
-				e.printStackTrace();
-			}
-			paused = true;
-		}else{
-			timer.notify();
-			paused = false;
-		}
-	}
+ActionListener engine = new ActionListener() {
+   Node node;
+   public void actionPerformed(ActionEvent evt) {
+      // / Issue a clock tick to each node in the node store.
+      for (int index = 0; index < store.getNumberOfNodes(); index++) {
+         node = store.getNodeByIndex(index);
+         node.clockTick();
+      }
+   }
+};
 
-	/**
-	 * Function that will stop a simulation
-	 * 
-	 * This function will stop a simulation indefinitely.
-	 * 
-	 * @author kennylmay
-	 * 
-	 * @param
-	 */
-	void stopSimluation() {
-		timer.stop();
-	}
-	
+/**
+ * Timer that will control the execution of the simulation
+ */
+Timer timer = new Timer(WAIT_TIME, engine);
 
-	/**
-	 * Function that returns the timer speed
-	 * 
-	 * This method will return the wait time.
-	 * 
-	 * @author kennylmay
-	 * 
-	 * @param 
-	 */
-	public int getSimSpeed() {
-		return timer.getDelay();
-	}
+/**
+ * Function that will start a simulation
+ * 
+ * This function will be the controlling function for all of the nodes
+ * 
+ * @author kennylmay
+ * 
+ * @param
+ */
+void runSimulation() {
+   timer.start();
 }
 
+/**
+ * Function that sets the timer speed
+ * 
+ * This method will allow the adjustment of the time interval(time between clock
+ * ticks) in seconds.
+ * 
+ * 
+ * @author kennylmay
+ * 
+ * @param speed
+ *           (int) The time in seconds that a simulation should pause between
+ *           ticks.
+ */
+void setSimulationSpeed(int speed) {
+   timer.setDelay(speed);
+}
+
+/**
+ * Function that will pause a simulation
+ * 
+ * This function will pause the simulation until the user decides to continue
+ * 
+ * @author kennylmay
+ * 
+ * @param
+ */
+void pauseSimluation() {
+   if (paused == false) {
+      try {
+         timer.wait();
+      } catch (InterruptedException e) {
+         // / May have to change this catch later
+         e.printStackTrace();
+      }
+      paused = true;
+   } else {
+      timer.notify();
+      paused = false;
+   }
+}
+
+/**
+ * Function that will stop a simulation
+ * 
+ * This function will stop a simulation indefinitely.
+ * 
+ * @author kennylmay
+ * 
+ * @param
+ */
+void stopSimluation() {
+   timer.stop();
+}
+
+NodeStore getNodeStore(){
+   return store;
+}
+
+/**
+ * Function that returns the timer speed
+ * 
+ * This method will return the wait time.
+ * 
+ * @author kennylmay
+ * 
+ * @param
+ */
+public int getSimSpeed() {
+   return timer.getDelay();
+}
+}
