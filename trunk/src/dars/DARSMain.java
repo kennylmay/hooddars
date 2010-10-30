1bf9163c62f69507edbd735840a10ce0c41580f8
memjjay <memjjay@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-10-30 15:14:38 +0000

    Populating DARSMain with some essential startup routines

--- trunk/src/dars/DARSMain.java
+++ trunk/src/dars/DARSMain.java
@@ -2,6 +2,8 @@
  * 
  */
 package dars;
+import logger.Logger;
+import dars.event.DARSEvent;
 import dars.gui.GUI;
 /**
  * @author Mike
@@ -13,7 +15,29 @@ public class DARSMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new GUI();
+		//Setup the logger to consume DARSEvents from both the input handler and
+		//the output handler. From henceforth, all DARSEvents that pass through
+		//the Input and Output handlers will be logged.
+		InputHandler.addInputConsumer(Logger.getInstance());
+		OutputHandler.addOutputConsumer(Logger.getInstance());
+		
+		
+		//Instantiate the simulator engine
+		SimEngine s = new SimEngine();
+		
+		//Name the simulator engine as an input consumer
+		//TODO: Have the simulator engine class implement the InputConsumer interface.
+		//InputHandler.addInputConsumer(s);
+
+		
+		//Instantiate the gui
+		GUI g = new GUI();
+		
+		//Name the GUI as an output consumer
+		OutputHandler.addOutputConsumer(g);
+		
+		
+		
 
 	}
 
--- trunk/src/dars/InputHandler.java
+++ trunk/src/dars/InputHandler.java
@@ -12,18 +12,18 @@ import dars.event.DARSEvent;
  * @author Mike
  *
  */
-public  class InputHandler {
+public class InputHandler {
   public static void dispatch(DARSEvent e){
 	for(InputConsumer c : inputConsumers){
 	  c.consumeInput(e);
     }
   } 
     
-  public void addInputConsumer(InputConsumer c) {
+  public static void addInputConsumer(InputConsumer c) {
 	  inputConsumers.add(c);
   }
   
-  public void removeInputConsumer(InputConsumer c) {
+  public static void removeInputConsumer(InputConsumer c) {
 	  inputConsumers.remove(c);
   }
   
