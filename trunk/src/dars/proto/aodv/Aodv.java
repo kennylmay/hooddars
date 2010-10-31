c8158044c512120b2ddcd780de9f5f2b8e7497c1
kresss <kresss@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-10-31 21:19:10 +0000

    More updates to RREQ as well as starting the notion of a RouteTable for AODV.
    Updated AODV RouteEntry with basic setters, getters, and constructors.
    
    Applied formating to Aodv.java and RouteEntry.java

--- trunk/src/dars/proto/aodv/Aodv.java
+++ trunk/src/dars/proto/aodv/Aodv.java
@@ -1,8 +1,12 @@
 package dars.proto.aodv;
 
+import java.util.HashMap;
+
 import dars.NodeAttributes;
 import dars.proto.Node;
 import dars.Message;
+import dars.proto.aodv.*;
+import dars.proto.aodv.RouteEntry.StateFlags;
 
 /**
  * AODV Node Class.
@@ -12,94 +16,95 @@ import dars.Message;
  */
 public class Aodv implements Node {
 
-	/**
-	 * Constants needed by the AODV Protocol
-	 */
+  /**
+   * Constants needed by the AODV Protocol
+   */
   public static final int TTL_START            = 5;
-  
-    
-	public static final int ACTIVE_ROUTE_TIMEOUT = 3000; // Milliseconds
-	public static final int HELLO_INTERVAL = 1000; // Milliseconds
-	public static final int NODE_TRAVERSAL_TIME = 40; // Milliseconds
+  public static final int NET_DIAMETER         = 35;
+  public static final int NODE_TRAVERSAL_TIME  = 2;
+  public static final int NET_TRAVERSAL_TIME   = 2 * NODE_TRAVERSAL_TIME
+                                                   * NET_DIAMETER;
+  public static final int PATH_DISCOVERY_TIME  = 2 * NET_TRAVERSAL_TIME;
 
-	public static final int ALLOWED_HELLO_LOSS = 2;
-	public static final int LOCAL_ADD_TTL = 2;
-	public static final int NET_DIAMETER = 35;
-	public static final int RREQ_RETRIES = 2;
-	public static final int RREQ_RATELIMIT = 10;
-	public static final int TIMEOUT_BUFFER = 2;
-	//public static final int TTL_START = 1;
-	public static final int TTL_INCREMENT = 2;
-	public static final int TTL_THRESHOLD = 7;
-	public static final int RERR_RATELIMIT = 10;
+  public static final int ACTIVE_ROUTE_TIMEOUT = 3000;                      // Milliseconds
+  public static final int HELLO_INTERVAL       = 1000;                      // Milliseconds
+  // public static final int NODE_TRAVERSAL_TIME = 40; // Milliseconds
 
-	public static final int MAX_REPAIR_TTL = (int)(0.3 * NET_DIAMETER);
-	public static final int NET_TRAVERSAL_TIME = 2 * NODE_TRAVERSAL_TIME
-			* NET_DIAMETER;
-	public static final int BLACKLIST_TIMEOUT = RREQ_RETRIES
-			* NET_TRAVERSAL_TIME;
-	public static final int MY_ROUTE_TIMEOUT = 2 * ACTIVE_ROUTE_TIMEOUT;
-	public static final int NEXT_HOP_WAIT = NODE_TRAVERSAL_TIME + 10;
-	public static final int PATH_DISCOVERY_TIME = 2 * NET_TRAVERSAL_TIME;
-	
-	// Ring Traversal Time is dependent on TTL_VALUE.
-	// Again I find myself not knowing what to do.
-	// public static final int RING_TRAVERSAL_TIME = 2 * NODE_TRAVERSAL_TIME
-	//		* (TTL_VALUE + TIMEOUT_BUFFER);
-	
-	/**
-	 * DELETE_PERIOD = K * max (ACTIVE_ROUTE_TIMEOUT, HELLO_INTERVAL)
-	 *     (K = 5 is recommended).
-	 */
-	public static final int DELETE_PERIOD = 5 * ACTIVE_ROUTE_TIMEOUT;
-	
-	// Don't think that MIN_REPAIR_TTL will really be needed.
-	// public static final int MIN_REPAIR_TTL = ??
-	
-	// Don't think that TTL_VALUE is really a constant.
-	// public static final int TTL_VALUE = ?? 
-	
-	/**
-	 * Functions that define the org.dars.proto.node interface.
-	 */
+  public static final int ALLOWED_HELLO_LOSS   = 2;
+  public static final int LOCAL_ADD_TTL        = 2;
 
-	/**
-	 * Send raw message into the network.
-	 * 
-	 * This function sends a preformed message into the network. A raw message
-	 * should be thought of as the bits on a wire not text.
-	 * 
-	 * @author kresss
-	 * @see org.dars.proto.node.sendRawMessage
-	 * 
-	 * @param message
-	 *            Preformated message to be transmitted.
-	 */
-	public void sendRawMessage(dars.Message message) {
+  public static final int RREQ_RETRIES         = 2;
+  public static final int RREQ_RATELIMIT       = 10;
+  public static final int TIMEOUT_BUFFER       = 2;
+  // public static final int TTL_START = 1;
+  public static final int TTL_INCREMENT        = 2;
+  public static final int TTL_THRESHOLD        = 7;
+  public static final int RERR_RATELIMIT       = 10;
 
-	}
+  public static final int MAX_REPAIR_TTL       = (int) (0.3 * NET_DIAMETER);
 
-	/**
-	 * Receive raw message from the network.
-	 * 
-	 * This function will receive a message from the network then determine what
-	 * type of message it is and call the appropriate message processing
-	 * routine.
-	 * 
-	 * @author kresss
-	 * @see org.dars.proto.node.receiveRawMessage
-	 * 
-	 * @param message
-	 *            Message from the network simulation engine.
-	 */
-	public void receiveRawMessage() {
+  public static final int BLACKLIST_TIMEOUT    = RREQ_RETRIES
+                                                   * NET_TRAVERSAL_TIME;
+  public static final int MY_ROUTE_TIMEOUT     = 2 * ACTIVE_ROUTE_TIMEOUT;
+  public static final int NEXT_HOP_WAIT        = NODE_TRAVERSAL_TIME + 10;
 
-	}
+  // Ring Traversal Time is dependent on TTL_VALUE.
+  // Again I find myself not knowing what to do.
+  // public static final int RING_TRAVERSAL_TIME = 2 * NODE_TRAVERSAL_TIME
+  // * (TTL_VALUE + TIMEOUT_BUFFER);
 
-	/**
-	 * Functions that extend the org.dars.proto.node interface to make it unique
-	 * to aodv.
-	 */
+  /**
+   * DELETE_PERIOD = K * max (ACTIVE_ROUTE_TIMEOUT, HELLO_INTERVAL) (K = 5 is
+   * recommended).
+   */
+  public static final int DELETE_PERIOD        = 5 * ACTIVE_ROUTE_TIMEOUT;
+
+  // Don't think that MIN_REPAIR_TTL will really be needed.
+  // public static final int MIN_REPAIR_TTL = ??
+
+  // Don't think that TTL_VALUE is really a constant.
+  // public static final int TTL_VALUE = ??
+
+  /**
+   * Functions that define the org.dars.proto.node interface.
+   */
+
+  /**
+   * Send raw message into the network.
+   * 
+   * This function sends a preformed message into the network. A raw message
+   * should be thought of as the bits on a wire not text.
+   * 
+   * @author kresss
+   * @see org.dars.proto.node.sendRawMessage
+   * 
+   * @param message
+   *          Preformated message to be transmitted.
+   */
+  public void sendRawMessage(dars.Message message) {
+
+  }
+
+  /**
+   * Receive raw message from the network.
+   * 
+   * This function will receive a message from the network then determine what
+   * type of message it is and call the appropriate message processing routine.
+   * 
+   * @author kresss
+   * @see org.dars.proto.node.receiveRawMessage
+   * 
+   * @param message
+   *          Message from the network simulation engine.
+   */
+  public void receiveRawMessage() {
+
+  }
+
+  /**
+   * Functions that extend the org.dars.proto.node interface to make it unique
+   * to aodv.
+   */
 
   /**
    * Generate and send a Route Request Message.
@@ -120,7 +125,7 @@ public class Aodv implements Node {
      * 
      */
 
-    /** 
+    /**
      * Message object that will be passed to SendRawMessage.
      */
     Message Msg;
@@ -142,113 +147,153 @@ public class Aodv implements Node {
     int MsgSrcSeqNum = ++this.LastSeqNum;
 
     /**
-     * Look up the last known Destination Sequence Number out of the route
-     * table. If no entry exists in the route table for the destination ID then
-     * set the destination sequence number to 0 and set the destination sequence
-     * unknown flag(U).
+     * Destination Route Table Entry This is used to determine what the last
+     * known sequence number for the destination and also if the source node has
+     * already requested a route for the destination.
      */
-    // TODO: Try to lookup the destination sequence number. Until then just do
-    // the 'else' part.
-    MsgDestSeqNum = 0;
-    MsgFlags += "U";
+    RouteEntry DestRouteEntry = RouteTable.get(MsgDestID);
+
+    /**
+     * If a RouteEntry does not exist for Destination ID then create one to
+     * record that a RREQ was sent. Otherwise inspect the entry.
+     */
+    if (DestRouteEntry == null) {
+      /**
+       * Create a new Route Entry in the Route Table to record that a RREQ has
+       * been sent.
+       */
+      DestRouteEntry = new RouteEntry(MsgDestID, StateFlags.RREQSENT,
+          CurrentTick + PATH_DISCOVERY_TIME);
+    } else {
+      /**
+       * There is already a RouteEntry in the RouteTable, but we are still
+       * processing a RREQ. WHY?
+       */
+      if (DestRouteEntry.getState() == StateFlags.RREQSENT) {
+        if (DestRouteEntry.getLifetime() > CurrentTick) {
+          /**
+           * A RREQ has already been sent for this node but a response has not
+           * yet been received. Do not send another RREQ.
+           */
+          return;
+        }
+      } else {
+        // TODO: There could be other possible StateFlags conditions at this
+        // point.
+        System.out.println("TODO: sendRREQ: StateFlags = "
+            + DestRouteEntry.getState().toString());
+      }
+
+    }
+
+    /**
+     * Look up the last known Destination Sequence Number out of the route
+     * table. If the destination sequence number == 0 and set the destination
+     * sequence unknown flag(U).
+     */
+    MsgDestSeqNum = DestRouteEntry.getSeqNum();
+    if (MsgDestSeqNum == 0) {
+      MsgFlags += "U";
+    }
 
     /**
      * Build the actual message string.
      */
     MsgStr = MsgType + '|' + MsgFlags + '|' + MsgTTL + '|' + MsgRREQID + '|'
         + MsgDestID + '|' + MsgDestSeqNum + '|' + MsgSrcID + '|' + MsgSrcSeqNum;
-    
-    Msg = new Message("ALL", MsgSrcID, MsgStr);
-    
+
+    Msg = new Message(Message.BCAST_STRING, MsgSrcID, MsgStr);
+
     sendRawMessage(Msg);
-    
+
   }
-  
-	/**
-	 * Receive and decode a Route Request Message.
-	 * 
-	 * Decode a Route Request Message as defined by RFC 3561 Section 5.2
-	 * 
-	 * @author kresss
-	 * 
-	 * @param
-	 */
-	void receiveRREQ() {
 
-	}
+  /**
+   * Receive and decode a Route Request Message.
+   * 
+   * Decode a Route Request Message as defined by RFC 3561 Section 5.2
+   * 
+   * @author kresss
+   * 
+   * @param
+   */
+  void receiveRREQ() {
 
-	/**
-	 * Send Route Error Message.
-	 * 
-	 * Send a route error message as defined by RFC 3561 Section 5.3
-	 * 
-	 * @author kresss
-	 * 
-	 * @param
-	 */
-	void sendREER() {
+  }
 
-	}
+  /**
+   * Send Route Error Message.
+   * 
+   * Send a route error message as defined by RFC 3561 Section 5.3
+   * 
+   * @author kresss
+   * 
+   * @param
+   */
+  void sendREER() {
 
-	/**
-	 * Send Route Acknoledgement Message.
-	 * 
-	 * Send a route ack message as defined by RFC 3561 Section 5.4
-	 * 
-	 * @author kresss
-	 * 
-	 * @param
-	 */
-	void sendRREPACK() {
+  }
 
-	}
-	
-	/**
-    * Implements the getAttributes function that is defined in the Node Class.
-    * 
-    * This function will return the attributes that are defined in the Node class.
-    * 
-    * @author mayk
-    * 
-    * @return NodeAttributes
-    * 
-    * @param
-    */
-	public NodeAttributes getAttributes(){  
-      return att;
-	}
-	
-	/**
-    * Implements the setAttributes function that is defined in the Node Class.
-    * 
-    * This function will update the attributes for the node.
-    * 
-    * @author mayk
-    * 
-    * @return void
-    * 
-    * @param NodeAttributes
-    */
-	public void setAttributes(NodeAttributes atts){
-	 this.att = atts;
-   }
-	
-	public void clockTick(){
-	   
-	}
-	
-	/**
-	 * Attributes member
-	 */
-	   NodeAttributes att = new NodeAttributes();
-	   
+  /**
+   * Send Route Acknoledgement Message.
+   * 
+   * Send a route ack message as defined by RFC 3561 Section 5.4
+   * 
+   * @author kresss
+   * 
+   * @param
+   */
+  void sendRREPACK() {
+
+  }
+
+  /**
+   * Implements the getAttributes function that is defined in the Node Class.
+   * 
+   * This function will return the attributes that are defined in the Node
+   * class.
+   * 
+   * @author mayk
+   * 
+   * @return NodeAttributes
+   * 
+   * @param
+   */
+  public NodeAttributes getAttributes() {
+    return att;
+  }
+
+  /**
+   * Implements the setAttributes function that is defined in the Node Class.
+   * 
+   * This function will update the attributes for the node.
+   * 
+   * @author mayk
+   * 
+   * @return void
+   * 
+   * @param NodeAttributes
+   */
+  public void setAttributes(NodeAttributes atts) {
+    this.att = atts;
+  }
+
+  public void clockTick() {
+
+  }
+
+  /**
+   * Attributes member
+   */
+  NodeAttributes                      att         = new NodeAttributes();
+
   /**
    * Route Request ID
    * 
    * The RREQ ID must be unique to each route request sent out by a node. It is
    * incremented immediately before a route request is generated.
    */
-  private int    LastRREQID = 0;
+  private int                         LastRREQID  = 0;
 
   /**
    * Node Sequence Number
@@ -257,7 +302,18 @@ public class Aodv implements Node {
    * control messages(RREQ, RREP, RERR) for a node. It is incremented
    * immediately before a protocol control message is generated.
    */
-  private int    LastSeqNum = 0;
- 
+  private int                         LastSeqNum  = 0;
 
+  /**
+   * Route Table
+   */
+  private HashMap<String, RouteEntry> RouteTable  = new HashMap<String, RouteEntry>();
+
+  /**
+   * Current Tick
+   * 
+   * Time is loosely defined in the simulation. This is the current tick count
+   * for the node.
+   */
+  int                                 CurrentTick = 0;
 }
--- trunk/src/dars/proto/aodv/RouteEntry.java
+++ trunk/src/dars/proto/aodv/RouteEntry.java
@@ -5,78 +5,220 @@ package dars.proto.aodv;
  * 
  * This class represents a single entry in a routing table.
  * 
- * From RFC 3561 Section 2 Paragraph 6 
- *
+ * From RFC 3561 Section 2 Paragraph 6
+ * 
  * @author kresss
  */
 public class RouteEntry {
+  /**
+   * @param destIP
+   * @param state
+   * @param lifetime
+   */
+  public RouteEntry(String destIP, StateFlags state, long lifetime) {
+    super();
+    DestIP = destIP;
+    State = state;
+    Lifetime = lifetime;
 
-	/** 
-	 * Valid States for a Route Entry
-	 */
-	public enum StateFlags {
-	    /**
-	     * The entry is believed Viable.
-	     */
-		VALID,
-		/**
-		 * The entry is known to be flawed but has not been removed yet.
-		 */
-		INVALID, 
-		/**
-		 * The entry is broken but a repair operation is possible.
-		 */
-		REPAIRABLE, 
-		/**
-		 * The entry is broken but the repair procedure is underway. 
-		 */
-		REPAIRING,
-		/**
-		 * The entry was valid but has expired but should not be deleted yet.
-		 * 
-		 * TODO: This state was not clearly called for in the RFC.
-		 * 
-		 * An expired routing table entry SHOULD NOT be expunged before
-		 * (current_time + DELETE_PERIOD).
-		 * 
-		 * Maybe this state should really be REPAIRABLE.
-		 */
-		EXPIRED
-	}
+    SeqNum = 0;
+    HopCount = 0;
+  }
 
-	
-	
-	// TODO: Need some code here.
-	
-	
-	/**
-	 * Destination IP Address for the Destination Node
-	 */
-	private String DestIP;
-	/**
-	 * Destination Sequence Number
-	 */
-	private int SeqNum;
-	/**
-	 * Current State of the Routing Table Entry
-	 */
-	private StateFlags State;
-	/**
-	 * Hop Count to the Destination Node
-	 */
-	private int HopCount;
-	/**
-	 * IP Address of the Next Hop Node on the path to the Destination Node.
-	 */
-	private String NextHopIP;
-	/**
-	 * List of "Precursor" Node's IP Addresses.
-	 * Simply put this is a list of Nodes that need to be notified if this route breaks.
-	 */
-	private String PrecursorIPs[];
-	/**
-	 * Time, in milliseconds for which this entry is to be considered valid.
-	 */
-	private long Lifetime;
+  /**
+   * @param destIP
+   * @param seqNum
+   * @param state
+   * @param hopCount
+   * @param nextHopIP
+   * @param precursorIPs
+   * @param lifetime
+   */
+  public RouteEntry(String destIP, int seqNum, StateFlags state, int hopCount,
+      String nextHopIP, String[] precursorIPs, long lifetime) {
+    super();
+    DestIP = destIP;
+    SeqNum = seqNum;
+    State = state;
+    HopCount = hopCount;
+    NextHopIP = nextHopIP;
+    PrecursorIPs = precursorIPs;
+    Lifetime = lifetime;
+  }
+
+  /**
+   * @return the destIP
+   */
+  public String getDestIP() {
+    return DestIP;
+  }
+
+  /**
+   * @param destIP
+   *          the destIP to set
+   */
+  public void setDestIP(String destIP) {
+    DestIP = destIP;
+  }
+
+  /**
+   * @return the seqNum
+   */
+  public int getSeqNum() {
+    return SeqNum;
+  }
+
+  /**
+   * @param seqNum
+   *          the seqNum to set
+   */
+  public void setSeqNum(int seqNum) {
+    SeqNum = seqNum;
+  }
+
+  /**
+   * @return the state
+   */
+  public StateFlags getState() {
+    return State;
+  }
+
+  /**
+   * @param state
+   *          the state to set
+   */
+  public void setState(StateFlags state) {
+    State = state;
+  }
+
+  /**
+   * @return the hopCount
+   */
+  public int getHopCount() {
+    return HopCount;
+  }
+
+  /**
+   * @param hopCount
+   *          the hopCount to set
+   */
+  public void setHopCount(int hopCount) {
+    HopCount = hopCount;
+  }
+
+  /**
+   * @return the nextHopIP
+   */
+  public String getNextHopIP() {
+    return NextHopIP;
+  }
+
+  /**
+   * @param nextHopIP
+   *          the nextHopIP to set
+   */
+  public void setNextHopIP(String nextHopIP) {
+    NextHopIP = nextHopIP;
+  }
+
+  /**
+   * @return the precursorIPs
+   */
+  public String[] getPrecursorIPs() {
+    return PrecursorIPs;
+  }
+
+  /**
+   * @param precursorIPs
+   *          the precursorIPs to set
+   */
+  public void setPrecursorIPs(String[] precursorIPs) {
+    PrecursorIPs = precursorIPs;
+  }
+
+  /**
+   * @return the lifetime
+   */
+  public long getLifetime() {
+    return Lifetime;
+  }
+
+  /**
+   * @param lifetime
+   *          the lifetime to set
+   */
+  public void setLifetime(long lifetime) {
+    Lifetime = lifetime;
+  }
+
+  /**
+   * Valid States for a Route Entry
+   */
+  public enum StateFlags {
+    /**
+     * The entry is believed Viable.
+     */
+    VALID,
+    /**
+     * The entry is known to be flawed but has not been removed yet.
+     */
+    INVALID,
+    /**
+     * The entry is broken but a repair operation is possible.
+     */
+    REPAIRABLE,
+    /**
+     * The entry is broken but the repair procedure is underway.
+     */
+    REPAIRING,
+    /**
+     * The entry was valid but has expired but should not be deleted yet.
+     * 
+     * TODO: This state was not clearly called for in the RFC.
+     * 
+     * An expired routing table entry SHOULD NOT be expunged before
+     * (current_time + DELETE_PERIOD).
+     * 
+     * Maybe this state should really be REPAIRABLE.
+     */
+    EXPIRED,
+    /**
+     * A route request has been sent for this destination ID, but no response
+     * has been received.
+     */
+    RREQSENT
+  }
+
+  // TODO: Need some code here.
+
+  /**
+   * Destination IP Address for the Destination Node
+   */
+  private String     DestIP;
+  /**
+   * Destination Sequence Number
+   */
+  private int        SeqNum;
+  /**
+   * Current State of the Routing Table Entry
+   */
+  private StateFlags State;
+  /**
+   * Hop Count to the Destination Node
+   */
+  private int        HopCount;
+  /**
+   * IP Address of the Next Hop Node on the path to the Destination Node.
+   */
+  private String     NextHopIP;
+  /**
+   * List of "Precursor" Node's IP Addresses. Simply put this is a list of Nodes
+   * that need to be notified if this route breaks.
+   */
+  private String     PrecursorIPs[];
+  /**
+   * Time, in milliseconds for which this entry is to be considered valid.
+   */
+  private long       Lifetime;
 
 }
