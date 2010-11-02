156b99794c49ef21762d91ef58b18120c1a21b7b
misael.marin <misael.marin@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-02 21:49:48 +0000

    Adding stub for send message dialog class

---
+++ src/dars/gui/SendNodeMessageDialog.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package dars.gui;
+
+import java.util.ArrayList;
+
+/**
+ * @author Jagriti
+ *
+ */
+public class SendNodeMessageDialog {
+
+  static boolean run(String fromNodeId, ArrayList<String> nodeIdList) {
+    
+    //Build a dialog
+    
+    //Filter out fromId from the dialog
+    
+    //run the dialog
+    
+    //dispatch a sendNodeMsg if OK, return true
+    
+    //else return false
+    
+    //placeholder
+    return true;
+  }
+                     
+}
