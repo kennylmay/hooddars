b8f3684c7fcdfe1ac745daf55a3409833b006840
misael.marin <misael.marin@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-02 21:43:00 +0000

    Added Parser class

---
+++ trunk/src/logger/Parser.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package logger;
+
+/**
+ * @author misael.marin
+ *
+ */
+public class Parser {
+
+  static void parse(String logFileLocation) {
+  ///Read in log file
+  
+  ///Parse the log into dars event
+  
+  //dispatch each dars event
+    
+  }
+  
+}
