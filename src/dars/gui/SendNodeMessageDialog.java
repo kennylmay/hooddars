47973d28e5fe118605cd73ac342569e0f8205504
jagriti.nanda <jagriti.nanda@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-03 20:28:25 +0000

    Still needs many changes

--- src/dars/gui/SendNodeMessageDialog.java
+++ src/dars/gui/SendNodeMessageDialog.java
@@ -17,6 +17,7 @@ import java.awt.event.*;
  */
 public class SendNodeMessageDialog {
 
+ 
   static boolean run(String fromNodeId, ArrayList<String> nodeIdList) {
     
     //Build a dialog
@@ -36,46 +37,65 @@ public class SendNodeMessageDialog {
                      
 
 
-public class Dialogbox extends JFrame implements ActionListener{
-public static final int WIDTH = 600;
-public static final int Height =400;
-public static final int Lines =30;
-public static final int CHAR_PER_LINE = 20;
-private String button22 = "Enter the Message";
-
-private JTextArea theText;
- public Dialogbox()
- { setSize(WIDTH , HEIGHT );
- setTitle("MEssage Box");
- Container contentPane = getContentPane();
- contentPane.setLayout(new BorderLayout());
- JPanel buttonpanel = new JPanel();
- buttonpanel.setBackground(Color.WHITE);
- buttonpanel.setLayout(new FlowLayout());
- JButton button1 = new JButton("OK");
- button1.addActionListener(this);
- buttonpanel.add(button1);
-  JButton button2 = new JButton("Cancel");
-  button2.addActionListener(this);
-  buttonpanel.add(button2);
- contentPane.add(buttonpanel, BorderLayout.SOUTH);
- JPanel textpanel = new JPanel();
- // JLabel textpane = new JLabel("Enter the Message");
- // textpane.add(textpane,BorderLayout.WEST);
- textpanel.setBackground(Color.gray);
- theText = new JTextArea(Lines, CHAR_PER_LINE);
- theText.setBackground(Color.WHITE);
- textpanel.add(theText);
- contentPane.add(textpanel,BorderLayout.CENTER);
- }
-public void actionPerformed(ActionEvent e){
-  }
-}
 
 }
   public static void inSendMsg(Message m, String sourceId, String destinationId)
   {
     }
-  }
   
+   }
+
+class Dialogbox1 extends JFrame implements ActionListener{
+  /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+  public static final int WIDTH = 600;
+  public static final int Height =400;
+  public static final int Lines =30;
+  public static final int CHAR_PER_LINE = 20;
+  public static String sourceId;
+  public static Message m;
+  public static String destinationId;
+ // private String button22 = "Enter the Message";
+
+  private JTextArea theText;
+   public Dialogbox1()
+   { setSize(WIDTH , HEIGHT );
+   setTitle("MEssage Box");
+   JFrame frame = new JFrame();
+   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   Container contentPane = getContentPane();
+   contentPane.setLayout(new BorderLayout());
+   JPanel buttonpanel = new JPanel();
+   buttonpanel.setBackground(Color.WHITE);
+   buttonpanel.setLayout(new FlowLayout());
+   JButton button1 = new JButton("OK");
+   button1.addActionListener(this);
+   buttonpanel.add(button1);
+    JButton button2 = new JButton("Cancel");
+    button2.addActionListener(this);
+    buttonpanel.add(button2);
+   contentPane.add(buttonpanel, BorderLayout.SOUTH);
+   JPanel textpanel = new JPanel();
+   // JLabel textpane = new JLabel("Enter the Message");
+   // textpane.add(textpane,BorderLayout.WEST);
+   textpanel.setBackground(Color.gray);
+   theText = new JTextArea(Lines, CHAR_PER_LINE);
+   theText.setBackground(Color.WHITE);
+   textpanel.add(theText);
+   contentPane.add(textpanel,BorderLayout.CENTER);
+   }
+  public void actionPerformed(ActionEvent e){
+    String actionCommand = e.getActionCommand();
+    if(actionCommand.equals("Ok"))
+      DARSEvent.inSendMsg(m, sourceId, destinationId);
+    else if (actionCommand.equals("Cancel"));
+      
+         
+    }
+  }
+
+
+
   
