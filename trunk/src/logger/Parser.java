348d5bb47754e19a8b33c15387169438ae635bc2
misael.marin <misael.marin@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-15 14:23:26 +0000

    Cleaned commented out code and modified the while statement comparison from equal to not equal.

--- trunk/src/logger/Parser.java
+++ trunk/src/logger/Parser.java
@@ -17,17 +17,6 @@ import dars.event.DARSEvent;
 public class Parser 
 {
   
-  public static void parseSetup(String logFileLocation) {
-    
-    
-    
-    //Read every line
-    //DARSEvent e = DARSEvent.parseLogStr(str);
-      //if Q = 0 and event is input type
-         //dispatch it
-    
-  }
-  
   public static void parseReplay(String logFileLocation) {
     DARSEvent d;
   ///Read in log file
@@ -37,14 +26,14 @@ public class Parser
       FileReader LogFile = new FileReader(logFileLocation);
       BufferedReader input =  new BufferedReader(LogFile);
       try {
-        String line = ""; 
+        String line = "";
         int lineNumber = 1;
         //go through lines
         while ((line = input.readLine()) != null){
           //if incorrect format, throw exception
           if(lineNumber == 1)
-          {
-            if(line.equals(DARSEvent.getLogHeader()))
+          { 
+            if(!line.equals(DARSEvent.getLogHeader()))
             {
               System.out.println("Error reading file");
               throw new Exception("Error: The CSV file is either corrupt or is in an incompatible format. Line # " + lineNumber);
@@ -67,86 +56,8 @@ public class Parser
     catch (Exception ex){
       ex.printStackTrace();
     }
-    
-    
-    
   }
   
-    /*public static void parse(String logFileLocation) 
-    {
-    ///Read in log file
-      try {
-        //use buffering, reading one line at a time
-        //FileReader always assumes default encoding is OK!
-        FileReader LogFile = new FileReader(logFileLocation);
-        BufferedReader input =  new BufferedReader(LogFile);
-        try {
-          String line = ""; 
-          int lineNumber = 1;
-          //go through lines
-          while (( line = input.readLine()) != null){
-            String[] details = line.split(",");
-
-            //if incorrect format, throw exception
-            if(details.length != 12)
-            {
-              System.out.println("Error reading file");
-              //throw new Exception("Error: The CSV file is either corrupt or is in an incompatible format. Line # " + lineNumber);
-            }
-            
-            if(lineNumber > 1) //skipping the header line
-            {   //Parse and dispatch the log's dars events
-              dispatchEvent(details);
-              //waiting(1); //waiting 1 second between each event.
-            }
-            //increment line number
-            lineNumber++;
-          }
-        }
-        finally {
-          input.close();
-        }
-      }
-      catch (IOException ex){
-        ex.printStackTrace();
-      }
-      
-    
-    } */
-    static void dispatchEvent(String[] lineEvent) {
-      //lineEvents array = eventType,nodeId,sourceId,destinationId,payload,informationalMessage,newSimSpeed,nodeX,nodeY,nodeRange
-      try
-      {
-        //dispatch each dars event
-        if(lineEvent[0].equals("OUT_ADD_NODE"))
-        { 
-          //waiting(1); //waiting 1 second between each event.
-          NodeAttributes newNode = new NodeAttributes();
-          newNode.id = lineEvent[1];
-          newNode.x = Integer.parseInt(lineEvent[7]); //node x
-          newNode.y = Integer.parseInt(lineEvent[8]); //node y
-          newNode.range = Integer.parseInt(lineEvent[9]);
-          
-          OutputHandler.dispatch(DARSEvent.outAddNode(newNode));
-        }
-        if(lineEvent[0].equals("OUT_MOVE_NODE"))
-        {
-          //waiting(1); //waiting 1 second between each event.
-          OutputHandler.dispatch(DARSEvent.outMoveNode(lineEvent[1],Integer.parseInt(lineEvent[7]),Integer.parseInt(lineEvent[8])));
-          
-        }
-        if(lineEvent[0].equals("OUT_DEL_NODE"))
-        {
-          //waiting(1); //waiting 1 second between each event.
-          OutputHandler.dispatch(DARSEvent.outDeleteNode(lineEvent[1]));
-        }
-      }
-      catch (Exception ex){
-        ex.printStackTrace();
-      }
-      
-  }
- 
   static void waiting (int n){
     
     long t0, t1;
