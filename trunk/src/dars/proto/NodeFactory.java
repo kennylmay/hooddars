803238f4d5543f4c379f0ef060f7ffb0afc5e77a
memjjay <memjjay@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-26 14:20:43 +0000

    Refactoring SimType out of DARSEvent and into a class called NodeFactory. NodeFactory is a minimal class that does two things:
    1. Defines the NodeTypes enum.
    2. Instantiates a new network node from a given NodeType and NodeAttributes.

---
+++ trunk/src/dars/proto/NodeFactory.java
@@ -0,0 +1,24 @@
+package dars.proto;
+
+import dars.NodeAttributes;
+import dars.proto.aodv.Aodv;
+import dars.proto.dsdv.Dsdv;
+
+/*
+ * Node Factory class.
+ * Instantiates a new node based on a supplied node type. Also contains 
+ * the definitions for node types (NodeType) that are used throughout DARS.
+ * 
+ * This class must be changed if you want to add a new network protocol. 
+ * 
+ */
+public class NodeFactory {
+  public enum NodeType { AODV, DSDV };
+  public static Node makeNewNode(NodeType nt, NodeAttributes na) {
+    switch(nt) {
+    case AODV : return new Aodv(na);
+    case DSDV : return new Dsdv();
+    default   : return null;
+    }
+  }
+}
