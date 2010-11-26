ed09d3e9e2e87e31a7dfe6e20603bb8c6e28ddec
kresss <kresss@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-26 22:25:37 +0000

    merge -r306:HEAD https://hooddars.googlecode.com/svn/branches/nodebaseclass into the trunk.

--- trunk/src/dars/proto/dsdv/Dsdv.java
+++ trunk/src/dars/proto/dsdv/Dsdv.java
@@ -6,7 +6,7 @@ import dars.Message;
 import dars.NodeAttributes;
 import dars.proto.Node;
 
-public class Dsdv implements Node {
+public class Dsdv extends Node {
 
   /**
    * **************************************************************************
@@ -148,15 +148,28 @@ public class Dsdv implements Node {
    */
 
   @Override
-  public void clockTick() {
+  public Message messageToNetwork() {
     // TODO Auto-generated method stub
-
+    return null;
   }
 
   @Override
-  public NodeAttributes getAttributes() {
+  public void messageToNode(Message message) {
     // TODO Auto-generated method stub
-    return null;
+    
+  }
+
+  @Override
+  public void newNarrativeMessage(String sourceID, String desinationID,
+      String messageText) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void clockTick() {
+    // TODO Auto-generated method stub
+    
   }
 
   @Override
@@ -166,58 +179,16 @@ public class Dsdv implements Node {
   }
 
   @Override
-  public boolean isPromiscuous() {
-    // TODO Auto-generated method stub
-    return false;
-  }
-
-  @Override
-  public Message messageToNetwork() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public void messageToNode(Message message) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void newNarrativeMessage(String sourceID, String desinationID,
-      String messageText) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setAttributes(NodeAttributes atts) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setPromiscuity(boolean value) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setRange(int range) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
-  public void setXY(int x, int y) {
-    // TODO Auto-generated method stub
-
-  }
-
-  @Override
   public void updateNodeDialog(JDialog dialog) {
     // TODO Auto-generated method stub
-
+    
   }
+
+
+  /**
+   * **************************************************************************
+   * *** Public Member Functions - Implement Node Interface
+   * **************************************************************************
+   */
 
 }
