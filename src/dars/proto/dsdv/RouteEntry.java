a5ed74c97827fdf9f3c9e31cb3394577c1c8661f
kresss <kresss@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-11-28 01:21:08 +0000

    The true start of DSDV.  Some basic control messages work.

--- src/dars/proto/dsdv/RouteEntry.java
+++ src/dars/proto/dsdv/RouteEntry.java
@@ -66,6 +66,76 @@ public class RouteEntry {
     HopCount = hopCount;
     NextHopIP = nextHopIP;
     InstTime = instTime;
+  }
+
+  /**
+   * @return the destIP
+   */
+  public String getDestIP() {
+    return DestIP;
+  }
+
+  /**
+   * @param destIP the destIP to set
+   */
+  public void setDestIP(String destIP) {
+    DestIP = destIP;
+  }
+
+  /**
+   * @return the seqNum
+   */
+  public int getSeqNum() {
+    return SeqNum;
+  }
+
+  /**
+   * @param seqNum the seqNum to set
+   */
+  public void setSeqNum(int seqNum) {
+    SeqNum = seqNum;
+  }
+
+  /**
+   * @return the hopCount
+   */
+  public int getHopCount() {
+    return HopCount;
+  }
+
+  /**
+   * @param hopCount the hopCount to set
+   */
+  public void setHopCount(int hopCount) {
+    HopCount = hopCount;
+  }
+
+  /**
+   * @return the nextHopIP
+   */
+  public String getNextHopIP() {
+    return NextHopIP;
+  }
+
+  /**
+   * @param nextHopIP the nextHopIP to set
+   */
+  public void setNextHopIP(String nextHopIP) {
+    NextHopIP = nextHopIP;
+  }
+
+  /**
+   * @return the instTime
+   */
+  public int getInstTime() {
+    return InstTime;
+  }
+
+  /**
+   * @param instTime the instTime to set
+   */
+  public void setInstTime(int instTime) {
+    InstTime = instTime;
   }  
   
   
