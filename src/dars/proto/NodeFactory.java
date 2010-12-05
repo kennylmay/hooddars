6cb4484335d3d58459198e6f6f1a84487483fc30
kresss <kresss@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-12-05 18:56:14 +0000

    A few updates.  Mostly formatting and comments.
    Created a super constructor for the Node class.  AODV and DSDV's constructors now call super().

--- src/dars/proto/NodeFactory.java
+++ src/dars/proto/NodeFactory.java
@@ -4,6 +4,10 @@ import dars.NodeAttributes;
 import dars.proto.aodv.Aodv;
 import dars.proto.dsdv.Dsdv;
 
+// Add new protocol import here.
+// import dars.proto.newproto.NewProto;
+
+
 /*
  * Node Factory class.
  * Instantiates a new node based on a supplied node type. Also contains 
@@ -13,6 +17,8 @@ import dars.proto.dsdv.Dsdv;
  * 
  */
 public class NodeFactory {
+  // Add the protocol Name to the Enumeration
+  // public enum NodeType { AODV, DSDV, NewProtoName };
   public enum NodeType { AODV, DSDV };
   public static Node makeNewNode(NodeType nt, NodeAttributes na) {
     if(nt == null || na == null) {
@@ -22,6 +28,8 @@ public class NodeFactory {
     switch(nt) {
     case AODV : return new Aodv(na);
     case DSDV : return new Dsdv(na);
+    // Create nodes of the new NodeType.
+    // case NewProtoName : return NewProto(na); 
     default   : return null;
     }
   }
