efd494743e00f78093b1cb9ecb182e8f8d2eab4d
memjjay <memjjay@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-12-08 02:00:45 +0000

    Reverting back the bolding work in the log area. JTextPanes are just too ridiculously resource intensive. Using the plain jane JTextArea instead (which does not allow for mixing bold and non-bold text) in favor of not bringing the application to a crawl. Perhaps when Java 7 rolls around there will be a more efficient text widget..

--- src/dars/gui/LogArea.java
+++ src/dars/gui/LogArea.java
@@ -4,15 +4,15 @@
 package dars.gui;
 
 import java.awt.BorderLayout;
+import java.math.BigInteger;
+
 import javax.swing.BorderFactory;
-import javax.swing.JEditorPane;
 import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
+
 import dars.Defaults;
 import dars.Utilities;
 
@@ -25,7 +25,6 @@ public class LogArea extends javax.swing.JPanel {
   /**
    * 
    */
-   
   private static final long serialVersionUID = 1L;
   public static String newline = System.getProperty("line.separator");
   
@@ -36,51 +35,41 @@ public class LogArea extends javax.swing.JPanel {
   public void appendLog(String logType, String log, long quantum) {
     
     //Construct the log line
-    String boldPart = Utilities.timeStamp() + " Q" + quantum + " : " + logType + " : ";
-    String normalPart = log + newline;
+    String line = (Utilities.timeStamp() + " Q" + quantum + " : " + logType + " : " + log + newline);
 
-    Document doc = textArea.getDocument();
-    try {
-      doc.insertString(doc.getLength(), boldPart, boldAttribute);
-      doc.insertString(doc.getLength(), normalPart, normalAttribute);
-    } catch (BadLocationException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    //Maintain the buffer
+    clampBuffer(line.length());
+    
+    //Append
+    textArea.append(line);
     
     //Move the caret to chase the log as it grows downward
     textArea.setCaretPosition(textArea.getDocument().getLength());
-    
-    //Maintain the buffer
-    clampBuffer();
+
     
   }
   
-  void clampBuffer()
+  void clampBuffer(int incomingDataSize)
   {
      Document doc = textArea.getDocument();
      //If the document is > buf size, trunc it down to 50 percent of buf size.
-     int overLength = doc.getLength() - Defaults.LOG_AREA_BUF_SIZE;
+     int overLength = doc.getLength() + incomingDataSize - Defaults.LOG_AREA_BUF_SIZE;
 
      if (overLength > 0)
      {
-       
         try {
           //Chomp off from 0 to the end of the line where offset LOG_AREA_BUF_SIZE/2 falls.
-          String chompLine = doc.getText(Defaults.LOG_AREA_BUF_SIZE/2, 150);
-          int chompStop = Defaults.LOG_AREA_BUF_SIZE/2 + chompLine.indexOf(newline) + 1;
-          
-          doc.remove(0, chompStop + 1);
+          doc.remove(0, textArea.getLineEndOffset(textArea.getLineOfOffset(Defaults.LOG_AREA_BUF_SIZE/2)) );
         } catch (BadLocationException e) {
           Utilities.showError("An error occurred while truncating the buffer in the LogArea. Please file a bug report.");
           System.exit(1);
-       }
+        }
      }
   }
 
 
   private JScrollPane jsp;
-  private JTextPane textArea = new JTextPane();
+  private JTextArea textArea = new JTextArea();
   
   public LogArea() {
     
@@ -102,16 +91,9 @@ public class LogArea extends javax.swing.JPanel {
   }
   
 
-  private SimpleAttributeSet boldAttribute = new SimpleAttributeSet();
-  private SimpleAttributeSet normalAttribute = new SimpleAttributeSet();
-  private void configureTextArea(JEditorPane ta) {
-    StyleConstants.setBold(boldAttribute, true);
-    StyleConstants.setFontFamily(boldAttribute, Defaults.FONT.getFamily());
-    StyleConstants.setFontFamily(normalAttribute, Defaults.FONT.getFamily());
-    
+  private void configureTextArea(JTextArea ta) {
     //not editable
     ta.setEditable(false); 
-    
   
   }
   
