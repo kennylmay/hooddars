a57c79acf3f4b4978afc3116f16a28f8b1e0e3a5
memjjay <memjjay@dd961b81-c10f-3219-f6fc-c6d06d5d0f82>
2010-12-09 20:31:47 +0000

    Catching file io errors properly (ie NO DISK SPACE LEFT) and then System.exit()ing in Logger. Formatting Log area too.

--- src/dars/gui/LogArea.java
+++ src/dars/gui/LogArea.java
@@ -4,6 +4,7 @@
 package dars.gui;
 
 import java.awt.BorderLayout;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.concurrent.LinkedBlockingQueue;
 import javax.swing.BorderFactory;
@@ -26,43 +27,58 @@ public class LogArea extends javax.swing.JPanel {
    * 
    */
 
-  private static final StringBuilder sb = new StringBuilder(65535);
+  private static final StringBuilder sb = new StringBuilder(
+                                            Defaults.LOG_AREA_BUF_SIZE);
+
   private class Appender extends Thread {
     public void run() {
       LinkedList<String> lineList = new LinkedList<String>();
-      
+
       while (true) {
         try {
-        lineList.clear();
-        sb.setLength(0);
-        lineList.add(lines.take());
-        synchronized(lines) {
-          lines.drainTo(lineList);
-        }
-        for (String l : lineList) {
-          if(l == null) continue;
-          
-          sb.append(l);
-        }
-        
-        // Append
-        textArea.append(sb.toString());
-        
-        // Maintain the buffer
-        clampBuffer();
-        // Move the caret to chase the log as it grows downward
-        textArea.setCaretPosition(textArea.getDocument().getLength());
-        
-        }
-        catch(Exception e)     {
-            continue;
+          lineList.clear();
+          sb.setLength(0);
+          lineList.add(lines.take());
+          synchronized (lines) {
+            lines.drainTo(lineList);
+          }
+
+          // Build the buffer from the end to the beginning
+          int bufSize = 0;
+          int strLen = 0;
+          String l;
+          Iterator<String> di = lineList.descendingIterator();
+          boolean shouldBreak = false;
+          while (di.hasNext() && !shouldBreak) {
+            l = di.next();
+            strLen = l.length();
+
+            // If there's no more room in the buffer, break out.
+            if (bufSize + strLen > Defaults.LOG_AREA_BUF_SIZE) {
+              shouldBreak = true;
+            } else {
+              sb.insert(0, l);
+              bufSize += strLen;
+
+            }
+          }
+
+          // Maintain the buffer
+          clampBuffer(bufSize);
+
+          // Append
+          textArea.append(sb.toString());
+
+          // Move the caret to chase the log as it grows downward
+          textArea.setCaretPosition(textArea.getDocument().getLength());
+        } catch (Exception e) {
+          continue;
         }
       }
     }
 
   }
-  
-  
+
   private static final long           serialVersionUID = 1L;
   public static String                newline          = System
                                                            .getProperty("line.separator");
@@ -77,23 +93,25 @@ public class LogArea extends javax.swing.JPanel {
     // Construct the log line
     String line = (Utilities.timeStamp() + " Q" + quantum + " : " + logType
         + " : " + log + "\n");
-    synchronized(lines) {
+    synchronized (lines) {
       lines.add(line);
     }
 
   }
 
-  void clampBuffer() {
+  void clampBuffer(int incomingBufSize) {
     Document doc = textArea.getDocument();
     // If the document is > buf size, trunc it down to 50 percent of buf size.
-    int overLength = doc.getLength() - Defaults.LOG_AREA_BUF_SIZE;
+    int overLength = doc.getLength() + incomingBufSize
+        - Defaults.LOG_AREA_BUF_SIZE;
 
     if (overLength > 0) {
       try {
         // Chomp off from 0 to the end of the line where offset
         // LOG_AREA_BUF_SIZE/2 falls.
-       textArea.replaceRange("", 0, textArea.getLineEndOffset(textArea
-           .getLineOfOffset(doc.getLength()/2)));
+        textArea.replaceRange("", 0, textArea.getLineEndOffset(textArea
+            .getLineOfOffset(Math.min(doc.getLength(), doc.getLength()
+                - doc.getLength() / 2 + incomingBufSize))));
       } catch (BadLocationException e) {
         Utilities
             .showError("An error occurred while truncating the buffer in the LogArea. Please file a bug report.");
@@ -107,8 +125,8 @@ public class LogArea extends javax.swing.JPanel {
 
   public LogArea() {
 
-    setBorder(BorderFactory.createTitledBorder(
-        BorderFactory.createEtchedBorder(), "Console", TitledBorder.CENTER,
+    setBorder(BorderFactory.createTitledBorder(BorderFactory
+        .createEtchedBorder(), "Console", TitledBorder.CENTER,
         TitledBorder.TOP, Defaults.BOLDFACED_FONT));
 
     // Setup the text area.
@@ -135,6 +153,5 @@ public class LogArea extends javax.swing.JPanel {
   public void clear() {
     textArea.setText("");
   }
-
 
 }
