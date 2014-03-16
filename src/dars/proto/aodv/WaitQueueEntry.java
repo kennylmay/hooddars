10adb27f4c30bdfda3fcf56ac98365ecba532c02
kresss <kresss@gmail.com>
2014-03-16 17:17:17 +0000

    Adding replay messages.

--- src/dars/proto/aodv/WaitQueueEntry.java
+++ src/dars/proto/aodv/WaitQueueEntry.java
@@ -13,9 +13,10 @@ class WaitQueueEntry {
   String DestinationID;
   String MsgString;
   int    TimeToLive;
+  boolean ReplayMessage;
 
   /**
-   * Constructor with all fields defined.
+   * Constructor with all normal fields defined.
    * 
    * @author kresss
    * 
@@ -31,5 +32,28 @@ class WaitQueueEntry {
     DestinationID = destinationID;
     MsgString = msgString;
     TimeToLive = timeToLive;
+    ReplayMessage = false;
   }
+  
+  /**
+   * Constructor with all fields defined. This includes the malicious replay message.
+   * 
+   * @author kresss
+   * 
+   * @param sourceID
+   * @param destinationID
+   * @param msgString
+   * @param timeToLive
+   * @param replayMessage
+   */
+  WaitQueueEntry(String sourceID, String destinationID, String msgString,
+      int timeToLive, boolean replayMessage) {
+    super();
+    SourceID = sourceID;
+    DestinationID = destinationID;
+    MsgString = msgString;
+    TimeToLive = timeToLive;
+    ReplayMessage = replayMessage;
+  }
+  
 }
